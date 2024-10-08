fd7309a6f407569f27498b2e136c34204c282868
Jeremiah Toh Rabino <jerryrabz01@gmail.com>
2024-10-08 10:30:29 +0800

    first edits

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,9 @@
+const AboutMe = () => {
+    return (
+        <div>
+            <p>Hello, I am Jerry Rabino, a third-year student passionate about coding and technology.</p>
+        </div>
+    );
+};
+
+export default AboutMe;
