0037976c485930682a87deaa32a0a6d59401f92d
Jeremiah Toh Rabino <jerryrabz01@gmail.com>
2024-10-10 09:48:58 +0800

    sign in/sign up

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, pgTableCreator, varchar } from "drizzle-orm/pg-core";
+
+export const createTable = pgTableCreator((name) => `heartcodeTraining_${name}`);
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
